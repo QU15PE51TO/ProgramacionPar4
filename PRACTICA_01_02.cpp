4924c78a1d1c9f3fe41fe842ee5bc2786bc19406
Kevin Quispe <kevin.quispe.m@ucb.edu.bo>
2026-08-18 20:54:50 -0400

    Adiciono el ejercicio 2 de la practica 1

---
+++ PRACTICA_01_02.cpp
@@ -0,0 +1,14 @@
+//Materia: Programacion 1 paralelo 4
+//Autor: Quispe Mamani Kevin Javier
+//Carrera del estudiante: ingenieria mecatronica
+//Fecha de creacion: 15/08/2026
+#include <iostream>
+using namespace std;
+int main()
+{
+    double precio;
+    cout << "introduce el precio del producto: ";
+    cin >> precio;
+    cout << "El resultado es: "<< precio + precio*0.13;
+    return 0;
+}
